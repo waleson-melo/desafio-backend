e5bf9722570f1ffb44bd3ae9d2dbc6c3c035287b
Waleson Melo <walesonmelo23@gmail.com>
2024-03-09 01:21:00 -0400

    adiciona os endpoits de cliente e produto

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,19 @@
 import { Router } from "express"
+import { createClienteController } from "./useCases/Cliente/CreateCliente"
+import { listProdutoController } from "./useCases/Produto/ListProduto"
 
 const router = Router()
 
 router.get("/", (request, response) => {
   return response.status(200).send({ message: "Hello World!" })
+})
+
+router.post("/clientes", (request, response) => {
+  return createClienteController.handle(request, response)
+})
+
+router.get("/produtos", (request, response) => {
+  return listProdutoController.handle(request, response)
 })
 
 export { router }
